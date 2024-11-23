a54d81a49652e864093baf6f61d77f80e00a425e
Itiellima <itiel.lima.cavalcante@gmail.com>
2024-11-23 16:13:02 -0300

    Sexto commit Beans Primary e annotations de Qualifiers

--- src/main/java/com/github/Itiellima/arquiteturaspring/montadora/configuration/MontadoraConfiguration.java
+++ src/main/java/com/github/Itiellima/arquiteturaspring/montadora/configuration/MontadoraConfiguration.java
@@ -4,6 +4,7 @@ import com.github.Itiellima.arquiteturaspring.montadora.Motor;
 import com.github.Itiellima.arquiteturaspring.montadora.TipoMotor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class MontadoraConfiguration {
@@ -20,6 +21,7 @@ public class MontadoraConfiguration {
     }
 
     @Bean(name ="motorEletrico")
+    @Primary
     public Motor motorEletrico(){
         var motor = new Motor();
         motor.setCavalos(110);
